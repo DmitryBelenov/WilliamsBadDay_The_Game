405498efff7a3e72775b63c14c6f051957e2518b
Dmitry Belenov <dmitrij_belenov@mail.ru>
2021-06-08 00:12:33 +0300

    More levels. Robo mentor

--- src/main/resources/tiles/level/ShipEnvironment.tsx
+++ src/main/resources/tiles/level/ShipEnvironment.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="ShipEnvironment" tilewidth="8" tileheight="8" tilecount="1024" columns="32">
- <image source="ShipEnvironment.png" width="256" height="256"/>
+<tileset version="1.5" tiledversion="1.6.0" name="ShipEnvironment" tilewidth="8" tileheight="8" tilecount="4096" columns="64">
+ <image source="ShipEnvironment.png" width="515" height="512"/>
 </tileset>
