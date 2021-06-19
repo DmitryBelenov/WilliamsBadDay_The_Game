6c04e2fe67005a1d3dbdfc37b52fa2c54eaaae45
Dmitry Belenov <dmitrij_belenov@mail.ru>
2021-06-19 22:52:21 +0300

    Improve lvl 5

---
+++ src/main/resources/tiles/level/Lab.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="Lab" tilewidth="8" tileheight="8" tilecount="10350" columns="115">
+ <image source="../../../../../../GameEnvironment/TiledFiles/png/Lab.png" width="921" height="720"/>
+</tileset>
